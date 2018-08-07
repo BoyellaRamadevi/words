c569d7716d45777e0c74870b9e31e93e5d49e19c
BoyellaRamadevi <41728410+BoyellaRamadevi@users.noreply.github.com>
2018-08-07 14:53:48 +0530

    Create words.java

---
+++ words.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java,util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 String s;
+         Scanner sc=new Scanner(System.in);
+         s=sc.nextLine();
+		int count = 1;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				count++;
+ 
+			}
+		}
+ 		System.out.println(count);
+	}
+	}
